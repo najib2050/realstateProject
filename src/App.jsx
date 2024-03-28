fa231b4b4a7c79057d9264f424407edcfe49bd2a
najib2050 <abdiwelinoor2@gmail.com>
2024-03-28 14:40:51 +0300

    extentions

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 export default function App() {
   return (
-    <div className="text-red-600 bg-black">
+    <div className=" bg-slate-300">
       <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
